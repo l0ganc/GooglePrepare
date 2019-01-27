b26c4308422a1b4e9e1f13ae86564e9ba6665d9b
l0ganc <jingyige@usc.edu>
2019-01-27 15:56:24 -0800

    image reverse bytes的补充

--- src/Occur4times/imageReverseBit.java
+++ src/Occur4times/imageReverseBit.java
@@ -14,6 +14,30 @@ package Occur4times;
  *
  *  可以用个hashmap存一下每个byte对应的反转byte，这样的操作就是128*8 常数
  */
+
+
+/**
+ *
+ * 第四轮也像一个欧洲来的。先给了一道题，flipImage(byte[] img, int width, int height)
+ * 把这个Byte Image给左右翻转。给的Input其实是个2D Array，
+ * 但是用1D表示(就是byte[] image，比如说给定width height, 那么原来i, j的点，在1D数组里，表示的是i * width + j)。
+ * 对于Byte Array每一行，做[1,2,3]变成[3,2,1]。
+ * 接着问我这个Assumption是啥？随便扯了一点，最后他说你假定是一个byte存一个Pixel。
+ * 那么现在改成Bit Image怎么弄？那么每个Bit翻转咯，写了一个Method去Bit翻转一个Byte。
+ * 接着他说，我给你一些Extra Memory，你怎么优化。我说那个256的Array把翻转结果存下来。
+ * 接着再问，你这个是一个Class，怎么初始化这个Array。讨论了好多初始化，最后他想要的答案是这个。
+ * 切记切记，Java还有这么奇怪的初始化方法。。。
+ *
+ * public class FlipImage {
+ *      private static final byte[] LOOKUP_ARRAY = INIT_LOOKUP_ARRAY();
+ *      private static byte[] INIT_LOOKUP_ARRAY() {
+ *      // Some code here
+ *          return new byte[256];
+ *      }
+ * }
+ *
+ *
+ */
 public class imageReverseBit {
 
 
