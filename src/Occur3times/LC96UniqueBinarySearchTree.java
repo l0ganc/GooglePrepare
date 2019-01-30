5a94c7771ced91a7d72780bcd544cee3eca4d075
l0ganc <jingyige@usc.edu>
2019-01-30 15:14:01 -0800

    LC 96 Unique Binary Search Tree

---
+++ src/Occur3times/LC96UniqueBinarySearchTree.java
@@ -0,0 +1,37 @@
+package Occur3times;
+
+/**
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+ *
+ * Example:
+ *
+ * Input: 3
+ * Output: 5
+ * Explanation:
+ * Given n = 3, there are a total of 5 unique BST's:
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ */
+public class LC96UniqueBinarySearchTree {
+    public static int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(numTrees(3));
+    }
+}
