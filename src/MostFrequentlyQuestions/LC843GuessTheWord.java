9d87d3918e07311994138b388cde513d99a8629c
l0ganc <jingyige@usc.edu>
2019-02-06 21:53:55 -0800

    LC843 guess the wor

---
+++ src/MostFrequentlyQuestions/LC843GuessTheWord.java
@@ -0,0 +1,37 @@
+package MostFrequentlyQuestions;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LC843GuessTheWord {
+    public static void findSecretWord(String[] wordlist, Master master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; i++) {
+            String word = wordlist[new Random().nextInt(wordlist.length)];
+            x = master.guess(word);
+            List<String> list = new ArrayList<>();
+
+            for (String s : wordlist) {
+                if (match(s, word) == x) {
+                    list.add(s);
+                }
+            }
+            wordlist = list.toArray(new String[list.size()]);
+        }
+    }
+
+    public int match(String s, String word) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == word.charAt(i)) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
+
+class Master{
+    public int guess(String word) {}
+}
