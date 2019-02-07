b7263e10eaf4c8411776fab9acb857631565f1b6
l0ganc <jingyige@usc.edu>
2019-02-07 09:34:11 -0800

    LC843 Guess the word 优化版

--- src/MostFrequentlyQuestions/LC843GuessTheWord.java
+++ src/MostFrequentlyQuestions/LC843GuessTheWord.java
@@ -1,9 +1,7 @@
 package MostFrequentlyQuestions;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class LC843GuessTheWord {
     public static void findSecretWord(String[] wordlist, Master master) {
@@ -30,6 +28,43 @@ public class LC843GuessTheWord {
         }
         return res;
     }
+
+    public static void findSecretWordII(String[] wordlist, Master master) {
+        List<String> list = new ArrayList<>();
+        for (String str : wordlist) {
+            list.add(str);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Map<String, Integer> zeroMatch = new HashMap<>();
+            for (String s1 : list) {
+                zeroMatch.putIfAbsent(s1, 0);
+                for (String s2 : list) {
+                    if (match(s1, s2) == 0) {
+                        zeroMatch.put(s1, zeroMatch.get(s1) + 1);
+                    }
+                }
+            }
+
+            Pair pair = new Pair("", 101);
+            for (String key : zeroMatch.keySet()) {
+                if (zeroMatch.get(key) < pair.freq) {
+                    pair = new Pair(key, zeroMatch.get(key));
+                }
+            }
+
+            int matchNum = master.guess(pair.key);
+            if (matchNum == 6) return;
+            List<String> tmp = new ArrayList<>();
+            for (String s : list) {
+                if (match(s, pair.key) == matchNum) {
+                    tmp.add(s);
+                }
+            }
+            list = tmp;
+        }
+
+    }
 }
 
 class Master{
@@ -37,3 +72,13 @@ class Master{
         return 1;
     }
 }
+
+class Pair {
+    String key;
+    int freq;
+    public Pair(String key, int freq) {
+        this.key = key;
+        this.freq = freq;
+    }
+}
+
