c85bd7097184b3b8fb432e2912d57ce7061c1c06
l0ganc <jingyige@usc.edu>
2019-02-07 21:03:48 -0800

    下围棋修改

--- src/Occur4times/LC130SurroundedRegions.java
+++ src/Occur4times/LC130SurroundedRegions.java
@@ -1,6 +1,5 @@
 package Occur4times;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
--- src/Occur4times/XiaWeiqi.java
+++ src/Occur4times/XiaWeiqi.java
@@ -5,6 +5,12 @@ package Occur4times;
  * follow up: 比如现在每个棋子都有一个status变量，代表这个地方是死是活，问题是当下一个棋子的时候如何更新棋子的status
  * 判断在一个棋盘上给定的棋子所在的片区有没有气，只要有一个气（空格），这片棋就没死
  *
+ * 给一个围棋棋盘，写一个function判断某一个白子是否是死子，即上下左右四个方向都被黑子包围。
+ * 也需要能判断一团白子是不是死子，即这团白子上下左右四个方向都被黑子包围。用bfs解。
+ * follow up是数出整个棋盘有多少个死掉的白子，和number of island类似. 我先说对于每个棋盘的白子，
+ * call 第一问的function，time n^4, 白人姐姐觉得太慢，我就把number of island解法说了一遍，满意，迅速写完。
+ * 她再指出我没有handle整个棋盘都是白子的case，我说是，我没想到，结束。
+ *
  *
  * BBBBB
  * BAA B
@@ -19,11 +25,8 @@ public class XiaWeiqi {
      * 判断一片棋子死活就是判断有没有空格邻居。类似Leetcode "number of islands".
      */
     public static boolean isAlive(int[][] matrix, int i, int j) {
-        if (i < 0 || i >= matrix.length || j < 0 || j > matrix[0].length) {
+        if (i < 0 || i >= matrix.length || j < 0 || j > matrix[0].length || matrix[i][j] == 0) {
             return false;
-        }
-        if (matrix[i][j] == 0) {
-            return true;
         }
 
         val = matrix[i][j];
@@ -31,21 +34,20 @@ public class XiaWeiqi {
     }
 
     private static boolean findEmptyNeignbour(int[][] matrix, int i, int j) {
-        if (i < 0 || i >= matrix.length || j < 0 || j > matrix[0].length ||
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length ||
         matrix[i][j] == Integer.MAX_VALUE || matrix[i][j] == -val) {
             return false;
         }
-
         if (matrix[i][j] == 0) {
             return true;
         }
 
         // set as visited
         matrix[i][j] = Integer.MAX_VALUE;
-        return (findEmptyNeignbour(matrix, i + 1, j) && matrix[i + 1][j] == val) ||
-                (findEmptyNeignbour(matrix, i - 1, j) && matrix[i - 1][j] == val) ||
-                (findEmptyNeignbour(matrix, i, j + 1) && matrix[i][j + 1] == val) ||
-                (findEmptyNeignbour(matrix, i, j - 1) && matrix[i][j - 1] == val);
+        return (findEmptyNeignbour(matrix, i + 1, j) ||
+                findEmptyNeignbour(matrix, i - 1, j) ||
+                findEmptyNeignbour(matrix, i, j + 1) ||
+                findEmptyNeignbour(matrix, i, j - 1));
     }
 
     public static void main(String[] args) {
